5760734d1a8362d0c311d224b2776501d04546e4
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-03 02:10:19 +0300

    ProfilePage component

---
+++ src/components/ProfilePage.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from "react"
+import "../styles/ProfilePage.css"
+import ProfileInfoCard from "./ProfileInfoCard";
+
+function ProfilePage() {
+    
+    return (
+        <div className="profilePage">
+            <section>
+                <ProfileInfoCard/>
+            </section>
+        </div>
+    );
+}
+
+export default ProfilePage;
