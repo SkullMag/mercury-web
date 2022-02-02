53d227e6114684d558dff535232ca859ac78ecb7
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-03 00:30:11 +0300

    Changed name of title

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import "../styles/LoginForm.css"
 import { useState } from "react"
+import { Link } from "react-router-dom"
 
 
-function LoginForm() {
+function LoginForm(props) {
 
     const [state, setState] = useState({
         username: "",
@@ -27,22 +28,23 @@ function LoginForm() {
     }
 
     async function login() {
-        const url = "http://127.0.0.1:8080/api/login"
+        const url = "http://localhost:8080/api/login"
         let response = await fetch(url, {method: "POST", body: JSON.stringify(state)});
         if (response.status === 200) {
             let json_data = await response.json();
             window.localStorage.setItem("token", json_data.token);
+            props.setTokenState({token: json_data.token});
         }
     }
 
     return (
         <div className="loginForm">
-            <h1 className="loginTitle">Login</h1>
+            <h1 className="loginTitle">Sign In</h1>
             <input type="text" placeholder="Username" onChange={usernameChanged} className="inputForm usernameInput" />
             <br/>
             <input type="password" placeholder="Password" onChange={passwordChanged} className="inputForm passwordInput" />
             <br/> 
-            <button className="loginButton" onClick={login}>Login</button>
+            <button className="loginButton" onClick={login}>Sign In</button>
         </div>
     );
 }
