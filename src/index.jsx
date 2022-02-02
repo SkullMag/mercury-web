68210631c9a49cc067dfd5a85be2822e0c09bb92
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-03 00:31:24 +0300

    Added new path to the router

--- src/index.jsx
+++ src/index.jsx
@@ -1,22 +1,34 @@
 import './index.css';
-import Definitions from './components/Definitions';
-import LoginForm from './components/LoginForm';
 import { render } from "react-dom";
 import { 
     BrowserRouter,
     Routes,
-    Route  
+    Route,
+    Navigate
 } from "react-router-dom";
+import { useState } from 'react';
 import Header from './components/Header';
+import ProfilePage from './components/ProfilePage';
+import Definitions from './components/Definitions';
+import LoginForm from './components/LoginForm';
 
 
-const rootElement = document.getElementById("root");
-render(<BrowserRouter>
+function Index() {
+    const [tokenState, setTokenState] = useState({
+        token: window.localStorage.getItem("token")
+    });
+    return (
+        <BrowserRouter>
             <Header />
             <Routes>
                 <Route path="/definitions" element={<Definitions />} />
-                <Route path="/login" element={<LoginForm />} />
+                <Route path="/account" element={tokenState.token === null ? <Navigate to="/login" /> : <ProfilePage />} />
+                <Route path="/login" element={<LoginForm setTokenState={setTokenState}/>} />
             </Routes>
-        </BrowserRouter>, 
-       rootElement);
+        </BrowserRouter>
+    );
+}
+
+const rootElement = document.getElementById("root");
+render(<Index />, rootElement);
 
