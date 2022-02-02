8ad782c47c8faa7a49f7f75b443f695f56ce03c6
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-03 00:29:44 +0300

    Handling definitions from new API

--- src/components/DefinitionsView.jsx
+++ src/components/DefinitionsView.jsx
@@ -7,14 +7,22 @@ import React from "react";
 function DefinitionsView(props) {
     var definitions = [];
     if (props.parentState.json_data != null) {
-        props.parentState.json_data.forEach((elem) => {
-            definitions.push(( <Definition partOfSpeech={capitalize(elem.partOfSpeech)} definition={elem.definition} example={elem.example} /> ));
-        });
+        var def = props.parentState.json_data.definitions
+        for (var partOfSpeech in def) {
+            definitions.push((<Definition partOfSpeech={capitalize(partOfSpeech)} definition={def[partOfSpeech]} example={def[partOfSpeech][0].example} />))
+            // def[partOfSpeech].forEach((definition) => {
+            //     definitions.push((<Definition partOfSpeech={capitalize(partOfSpeech)} definition={definition.definition} example={definition.example} />))
+            // })
+        }
+        // props.parentState.json_data.definitions.forEach((elem) => {
+        //     definitions.push(( <Definition partOfSpeech={capitalize(elem.partOfSpeech)} definition={elem.definition} example={elem.example} /> ));
+        // });
     }
     return (
         <div className="DefinitionsView">
             {definitions.length > 0 ? definitions : <p id="definition"><span id="partOfSpeech">Try to search for another word</span></p>}
         </div>
+        
     );
 }
 
