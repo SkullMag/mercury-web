925707aea3bab17014064b960c7a56f0605fc14c
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-07 20:53:54 +0300

    Created logout button

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,13 +1,24 @@
 import "../styles/ProfilePage.css"
 import ProfileInfoCard from "./ProfileInfoCard";
 import React from "react";
+import { useDispatch } from "react-redux";
+import { logoutAction } from "../store/reducers/auth";
+import { useNavigate } from "react-router-dom";
 
 function ProfilePage() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    function logout() {
+        dispatch(logoutAction());
+        navigate("/login");
+    }
     
     return (
         <div className="profilePage">
             <section>
                 <ProfileInfoCard/>
+                <button onClick={logout} className="card logoutButton">Log Out</button>
             </section>
         </div>
     );
