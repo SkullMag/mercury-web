93f0c6beba1a2ecfa4899d7d3564fdf523b46b0f
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-08 08:45:35 +0300

    Added alt property for profile image

--- src/components/ProfileInfoCard.jsx
+++ src/components/ProfileInfoCard.jsx
@@ -7,7 +7,7 @@ function ProfileInfoCard() {
     const authState = useSelector((state) => state.auth);
     return (
         <div className="card profileInfoCard">
-            <img className="userProfilePicture" src={authState.username !== "" ? "http://localhost:8080/api/getUserProfilePicture/" + authState.username : ""} />
+            <img className="userProfilePicture" alt="Profile image" src={authState.username !== "" ? "http://localhost:8080/api/getUserProfilePicture/" + authState.username : ""} />
             <h1 className="fullname">{authState.fullname}</h1>
             <p className="username">@{authState.username}</p>
             <i><q className="profileBio">{authState.profileBio}</q></i>
