b62dfc2506ff7653b27b2421cf1a8c8ad9d90b32
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-08 08:43:08 +0300

    Added function for email validation

--- src/utils.js
+++ src/utils.js
@@ -14,4 +14,12 @@ function pronounceWord(url) {
     player.play()
 }
 
-export {requestWord, capitalize, pronounceWord};
+function validateEmail(email) {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+}
+
+export {requestWord, capitalize, pronounceWord, validateEmail};
