5bbd3f3fe1b0e6fa7d2685b332166bce1c86b118
Олег Рыбалко <rybalko.oleg.123@mail.ru>
2022-02-10 15:36:39 +0300

    Created placeholder image

--- src/components/ProfileInfoCard.jsx
+++ src/components/ProfileInfoCard.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "../styles/ProfileInfoCard.css"
 import { useSelector } from "react-redux";
+import { SERVER_IP } from "../constants";
 
 
 function ProfileInfoCard() {
     const authState = useSelector((state) => state.auth);
+    var image = ""
+    if (authState.username !== "") {
+        image = "https://www.tinygraphs.com/isogrids/" + authState.username + "?theme=bythepool&numcolors=4&size=200&fmt=svg";
+    }
+
     return (
         <div className="card profileInfoCard">
-            <img className="userProfilePicture" alt="Profile" src={authState.username !== "" ? "http://localhost:8080/api/getUserProfilePicture/" + authState.username : ""} />
+            <img className="userProfilePicture" alt="Profile" src={image} />
             <h1 className="fullname">{authState.fullname}</h1>
             <p className="username">@{authState.username}</p>
             {authState.profileBio !== "" ? <i><q className="profileBio">{authState.profileBio}</q></i> : null }
