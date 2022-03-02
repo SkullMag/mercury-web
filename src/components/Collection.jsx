d63b3a4cbd87d7f01c146ab886756369ff5ce72a
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-02 09:47:54 +0300

    Added profile image to collection card

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import "../styles/Collection.css"
-import { capitalize } from "../utils.js";
+import { TINYGRAPH_IP } from "../constants";
+import { useSelector } from "react-redux";
 
 
 function Collection(props) {
 
     const navigate = useNavigate();
+    const authState = useSelector(state => state.auth)
 
     function openCollection() {
         navigate("/collections/" + props.authorUsername + "/" + props.collectionName)
@@ -16,17 +18,18 @@ function Collection(props) {
         <div className="card collection">
             <div className="card collectionDetails" onClick={openCollection}>
                 <p className="collectionName">
-                    {capitalize(props.collectionName)}
+                    {props.collectionName}
                 </p>
                 <p className="wordCount">
                     {props.wordCount === 1 ? props.wordCount + " word" : props.wordCount + " words"}
                 </p>
-                <svg className="chevron" xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 16 16">
-                <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
+                <svg className="chevron" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
                 </svg>
             </div>
             <div className="card collectionFeatures">
-                <p className="authorUsername">Created by <Link to="/account/">{"@" + props.authorUsername}</Link></p>
+                <img src={TINYGRAPH_IP + "/spaceinvaders/" + authState.username + "?theme=bythepool&numcolors=4&size=200&fmt=svg"} alt="account" className="profilePicture" /> 
+                <Link to="/account/">{"@" + props.authorUsername}</Link>
             </div>
         </div>
     );
