eec357a2ad7de0ac1d29422ebad7df3947a97b62
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-19 00:42:03 +0300

    Created hamburger icon for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,29 @@ import logo from "../assets/1024.png";
 import "../styles/Header.css";
 import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { HamburgerIcon } from "../icons/HamburgerIcon";
 
 
-function Header(props) {
+function Header() {
     const location = useLocation();
     const { t } = useTranslation("navbar")
+    const [isResponsive, setResponsive] = React.useState(false)
+
     return (
-        <nav className="Header">
-            <img alt="logo" src={logo} height="32px" id="logo" />
-            <p className="titleName">Mercury</p> 
-            <Link className={location.pathname === "/dictionary" ? "pageLink active" : "pageLink"} to="/dictionary">{t("dictionary")}</Link>
-            <Link className={location.pathname === "/collections" ? "pageLink active" : "pageLink"} to="/collections">{t("collections")}</Link>
-            <Link className={location.pathname === "/account" ? "pageLink active" : "pageLink"} to="/account">{t("account")}</Link>
+        <nav className={isResponsive ? "Header responsive" : "Header"}>
+            <div style={{display: "flex"}} className="logoTitle">
+                <img alt="logo" src={logo} height="32px" id="logo" />
+                <p className="titleName">Mercury</p> 
+            </div>
+            <div className="linksToPages">
+                <Link className={location.pathname === "/dictionary" ? "pageLink active" : "pageLink"} to="/dictionary">{t("dictionary")}</Link>
+                <Link className={location.pathname === "/collections" ? "pageLink active" : "pageLink"} to="/collections">{t("collections")}</Link>
+                <Link className={location.pathname === "/account" ? "pageLink active" : "pageLink"} to="/account">{t("account")}</Link>
+            </div>
+            
+            <button className="hamburger" onClick={() => setResponsive(!isResponsive)}>
+                <HamburgerIcon width="30px" height="30px" fill="white" />
+            </button>
         </nav>
     );
 }
---
+++ src/icons/HamburgerIcon.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export const HamburgerIcon = ({ width, height, fill, hamburgerClassName }) => { 
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height} fill={fill} className={"bi bi-list " + hamburgerClassName} viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+        </svg>
+    )
+}
