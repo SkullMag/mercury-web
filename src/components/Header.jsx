2bb80ad3d284fd70c0aad337fe77e754f186a350
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-19 03:27:52 +0300

    Created beautiful animation for hamburger icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,26 @@ import logo from "../assets/1024.png";
 import "../styles/Header.css";
 import { Link, useLocation } from "react-router-dom";
 import { useTranslation } from "react-i18next";
-import { HamburgerIcon } from "../icons/HamburgerIcon";
+import HamburgerButton from "./HamburgerButton";
 
 
 function Header() {
     const location = useLocation();
     const { t } = useTranslation("navbar")
     const [isResponsive, setResponsive] = React.useState(false)
+    const [isExpanded, setExpanded] = React.useState(false)
+
+    function toggleResponsive() {
+        setExpanded(!isExpanded)
+        if (isResponsive) {
+            setTimeout(() => setResponsive(!isResponsive), 600)
+        } else {
+            setResponsive(!isResponsive)
+        }
+    }
 
     return (
-        <nav className={isResponsive ? "Header responsive" : "Header"}>
+        <nav className={isResponsive ? (isExpanded ? "Header responsive expanded" : "Header responsive") : "Header"}>
             <div style={{display: "flex"}} className="logoTitle">
                 <img alt="logo" src={logo} height="32px" id="logo" />
                 <p className="titleName">Mercury</p> 
@@ -23,9 +33,10 @@ function Header() {
                 <Link className={location.pathname === "/account" ? "pageLink active" : "pageLink"} to="/account">{t("account")}</Link>
             </div>
             
-            <button className="hamburger" onClick={() => setResponsive(!isResponsive)}>
+            <HamburgerButton toggleResponsive={toggleResponsive}/>
+            {/* <button className="hamburger" onClick={() => setResponsive(!isResponsive)}>
                 <HamburgerIcon width="30px" height="30px" fill="white" />
-            </button>
+            </button> */}
         </nav>
     );
 }
