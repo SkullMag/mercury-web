f6dc1240ecf6232727283b6cf9acb3ea7244df18
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-27 13:23:13 +0300

    Changed url

---
+++ src/constants.js
@@ -0,0 +1,7 @@
+// Production
+export const SERVER_IP = "https://217.28.228.66:9090"
+export const TINYGRAPH_IP = "http://217.28.228.66:9191"
+
+// Test
+// export const TINYGRAPH_IP = "http://localhost:9191"
+// export const SERVER_IP = "http://localhost:8080"
