4eeb16d17730ab35f404505bc2b1da5c8cc34d76
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-28 21:12:50 +0300

    http -> https

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const SERVER_IP = "http://www.mercurydict.com:9090"
+export const SERVER_IP = "https://www.mercurydict.com:9090"
 export const TINYGRAPH_IP = "https://www.mercurydict.com:9191"
