4732a0680c667c871f579b56b0ff07676a4e63d1
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-28 20:06:02 +0300

    Deleted header buttons when no words in collection

--- src/components/CollectionWordsHeader.jsx
+++ src/components/CollectionWordsHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useTranslation } from "react-i18next"
+import { useSelector } from "react-redux"
 import { useNavigate } from "react-router-dom"
 import { CollectionWordsContext } from "../context/CollectionWordsContext"
 import "../styles/CollectionWordsHeader.css"
@@ -8,6 +9,7 @@ export default function CollectionWordsHeader({ authorUsername, collectionName }
     const { t } = useTranslation("collectionWords")
     const { isEditing, toggleEditing, collectionWords } = React.useContext(CollectionWordsContext)
     const navigate = useNavigate()
+    const authState = useSelector(state => state.auth)
 
     function openLearningWindow() {
         navigate("/learn/" + authorUsername + "/" + collectionName, {
@@ -17,14 +19,14 @@ export default function CollectionWordsHeader({ authorUsername, collectionName }
 
     return (
         <>
+        <div className="collectionWordsHeaderButtons">
+            {collectionWords && collectionWords.length > 0 && (<button className="learnWordsButton" onClick={openLearningWindow}>{t("learnButton")}</button>)}
+            {collectionWords && collectionWords.length > 0 && authorUsername === authState.username && (<button className="selectCollectionButton" onClick={toggleEditing}>
+                {isEditing ? t("cancelEditingButton", {ns: "collections"}) : t("startEditingButton", {ns: "collections"})}
+            </button>)}
+        </div>
         <div className="card collectionHeader">
             <p className="collectionTitle">{collectionName}</p>
-        </div>
-        <div className="collectionWordsHeaderButtons">
-        <button className="learnWordsButton" onClick={openLearningWindow}>{t("learnButton")}</button>
-            <button className="selectCollectionButton" onClick={toggleEditing}>
-                {isEditing ? t("cancelEditingButton", {ns: "collections"}) : t("startEditingButton", {ns: "collections"})}
-            </button>
         </div>
         </>
     )
