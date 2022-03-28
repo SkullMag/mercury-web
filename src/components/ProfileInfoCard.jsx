f5c9bceffc5e0ef53cc85491ac1f4aeb2d3ace7a
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-28 20:56:12 +0300

    Show other users profile cards

--- src/components/ProfileInfoCard.jsx
+++ src/components/ProfileInfoCard.jsx
@@ -1,23 +1,46 @@
 import React from "react";
 import "../styles/ProfileInfoCard.css"
 import { useSelector } from "react-redux";
-import { TINYGRAPH_IP } from "../constants";
+import { SERVER_IP, TINYGRAPH_IP } from "../constants";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 
 function ProfileInfoCard() {
-    const authState = useSelector((state) => state.auth);
-    let image = "";
-
-    if (authState.username !== "") {
-        image = TINYGRAPH_IP + "/spaceinvaders/" + authState.username + "?theme=bythepool&numcolors=4&size=200&fmt=svg";
-    }
+    const authState = useSelector((state) => state.auth)
+    const { username } = useParams()
+    const [userData, setUserData] = React.useState({
+        username: "",
+        isSubscribed: "",
+        fullname: "",
+        profileBio: "",
+        image: ""
+    })
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch([SERVER_IP, "api", "getUserData", authState.token, username].join("/"))
+            if (response.ok) {
+                let data = await response.json()
+                setUserData({
+                    username: data.username,
+                    fullname: data.fullname,
+                    profileBio: data.profileBio,
+                    isSubscribed: data.isSubscribed,
+                    image: TINYGRAPH_IP + "/spaceinvaders/" + (data.username) + "?theme=bythepool&numcolors=4&size=200&fmt=svg"
+                })
+            }
+        }
+        if (authState.username !== "") {
+            fetchData()
+        }
+    }, [authState.username])
 
     return (
         <div className="card profileInfoCard">
-            <img className="userProfilePicture" alt="Profile" src={image} />
-            <h1 className="fullname">{authState.fullname}</h1>
-            <p className="username">@{authState.username}</p>
-            {authState.profileBio !== "" ? <i><q className="profileBio">{authState.profileBio}</q></i> : null }
+            <img className="userProfilePicture" alt="Profile" src={userData.image} />
+            <h1 className="fullname">{userData.fullname}</h1>
+            <p className="username">@{userData.username}</p>
+            {userData.profileBio !== "" ? <i><q className="profileBio">{userData.profileBio}</q></i> : null }
         </div>
     );
 }
--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -3,29 +3,30 @@ import ProfileInfoCard from "./ProfileInfoCard";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logoutAction } from "../store/slices/auth";
-import { Navigate, useNavigate } from "react-router-dom";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 function ProfilePage() {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const authState = useSelector((state) => state.auth);
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+    const { username } = useParams()
+    const authState = useSelector((state) => state.auth)
     const { t } = useTranslation("auth")
 
     if (authState.token === "") {
-        return (<Navigate to="/login" />);
+        return (<Navigate to="/login" />)
     }
 
     function logout() {
-        dispatch(logoutAction());
-        navigate("/login");
+        dispatch(logoutAction())
+        navigate("/login")
     }
     
     return (
         <div className="profilePage">
             <section>
-                <ProfileInfoCard/>
-                <button onClick={logout} className="card logoutButton">{t("logoutButton")}</button>
+                {authState.username !== "" && (<ProfileInfoCard username={username}/>)}
+                {authState.username === username && (<button onClick={logout} className="card logoutButton">{t("logoutButton")}</button>)}
             </section>
         </div>
     );
