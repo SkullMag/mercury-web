7f6ed66a313e6cf43d603c9842f6f141b4b349c9
SkullMag <rybalko.oleg.123@mail.ru>
2022-03-30 09:02:27 +0300

    Added types

--- src/store/api/collections.js
+++ src/store/api/collections.js
@@ -1,14 +1,35 @@
-import {mercuryApi} from "./index"
+import { mercuryApi } from "./index"
 
 const collections = mercuryApi.injectEndpoints({
     endpoints: (builder) => ({
         getPublicCollections: builder.query({
-            query: () => "getCollections"
+            query: () => "getCollections",
+            providesTags: ["PublicCollection"]
         }),
         getLocalCollections: builder.query({
-            query: (token, username) => `getCollections/${token}/${username}`
+            query: (token, username) => `getCollections/${token}/${username}`,
+            providesTags: ["PublicCollection"]
+        }),
+        deleteCollection: builder.mutation({
+            query: (token, collectionName) => ({
+                url: `deleteCollection/${token}/${collectionName}`,
+                method: "DELETE"
+            }),
+            invalidatesTags: ["LocalCollection", "PublicCollection"]
+        }),
+        createCollection: builder.mutation({
+            query: ({token, collectionName}) => ({
+                url: `createCollection/${token}/${collectionName}`,
+                method: "POST"
+            }),
+            invalidatesTags: ["LocalCollection", "PublicCollection"]
         })
     })
 })
 
-export const { useGetLocalCollectionsQuery, useGetPublicCollectionsQuery } = collections
+export const { 
+    useGetLocalCollectionsQuery, 
+    useGetPublicCollectionsQuery,
+    useDeleteCollectionMutation,
+    useCreateCollectionMutation, 
+} = collections
--- src/store/api/index.js
+++ src/store/api/index.js
@@ -2,6 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const mercuryApi = createApi({
     reducerPath: 'mercuryApi',
+    tagTypes: ["LocalCollection", "PublicCollection"],
     baseQuery: fetchBaseQuery({ baseUrl: 'https://www.mercurydict.com:9090/api/' }),
     endpoints: () => ({}),
   })
